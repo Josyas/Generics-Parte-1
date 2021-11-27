03d0f5f5336e219e1eca90607676674238de3720
Josias <87546094+Josyas@users.noreply.github.com>
2021-11-26 22:55:36 -0300

    up arquivos

---
+++ GenericsPt1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericsPt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printservice printservice = new Printservice();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i=0; i<n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printservice.AddValue(x);
+            }
+
+            printservice.Print();
+            Console.WriteLine("First: " + printservice.First());
+        }
+    }
+}
